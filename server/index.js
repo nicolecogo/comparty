b964a3df667f80fb8c35584612b7899eb8843b31
Nicole Carrion Cogo <nicolecogo@gmail.com>
2020-04-18 13:22:23 +0200

    fix: using cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
+const cors = require('cors');
 
 const router = require('./router');
 
@@ -8,6 +9,7 @@ const SERVER_URL = process.env.SERVER_BASE_URL || 'http://localhost';
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(session({
   secret: 'keyboard cat',
