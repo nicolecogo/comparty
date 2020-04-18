4838899db992051b0da794399eed7a0a8ff8e19d
Nicole Carrion Cogo <nicolecogo@gmail.com>
2020-04-18 13:21:32 +0200

    fix: authorization working with client

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,5 @@
 
 module.exports.authenticate = (req, res) => {
-  //TODO send the user profile back to client instead of redirecting
-  // res.send(req.user);
-  res.redirect('/authenticated');
+  //TODO redirect directly to client from passport spotify
+  res.redirect(`http://localhost:3000/login?token=${req.user.accessToken}`);
 };
--- server/router.js
+++ server/router.js
@@ -27,7 +27,7 @@ function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect('/');
+  res.sendStatus(401)
 }
 
 module.exports = router;
