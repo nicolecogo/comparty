242e74965d485171a4cda4e65e6f72fda5737ce5
Nicole Carrion Cogo <nicolecogo@gmail.com>
2020-04-21 17:02:06 +0200

    feat: implemented sockets to synchronize plaback

---
+++ client/src/services/SocketClient.js
@@ -0,0 +1,48 @@
+import io from 'socket.io-client';
+import ServerClient from './ServerClient';
+
+class SocketClient {
+  static partyCode = '123456qwerty';
+
+  //connect to socket
+  static socket = io(ServerClient._BASE_URL);
+  
+  //set up socket listeners
+  static socketListeners(partyCode, { updatePlaylist, updatePlayback }) {
+    this.socket.on('connect', function() {
+      console.log('socket connected');
+    });
+    this.socket.on('disconnect', function(){
+      console.log('socket disconnected')
+    });
+    //listen for changes on playlist
+    this.socket.on(`from:${this.partyCode}:updatePlaylist`, playlist => {
+      console.log('msg from server updated playlist', playlist);
+      updatePlaylist(playlist);
+    });
+    //listen for changes on playback
+    this.socket.on(`from:${this.partyCode}:updatePlayback`, ({action, playback}) => {
+      console.log('msg from server updated playback', action, playback);
+      updatePlayback(playback, action);
+    });
+  }
+
+  //notify the playlist has changed
+  static sendPlaylistChange(updatedPlaylist) {
+    this.send(`updatePlaylist`, updatedPlaylist);
+  }
+
+  //notify the playback has changed
+  static sendPlaybackChange(updatedPlayback, action) {
+    this.send(`updatePlayback`, {action, playback: updatedPlayback});
+  }
+  
+  //generic method for emitting messages
+  static send (endpoint, data) {
+    if(!this.partyCode) new Error('Party code not available');
+    if(!this.socket) new Error('Socket not available');
+    this.socket.emit(`from:${this.partyCode}:${endpoint}`, data);
+  }
+}
+
+export default SocketClient;
--- server/index.js
+++ server/index.js
@@ -3,13 +3,22 @@ const session = require('express-session');
 const passport = require('passport');
 const cors = require('cors');
 
+const app = express();
+
 const router = require('./router');
 
 const SERVER_URL = process.env.SERVER_BASE_URL || 'http://localhost';
+const CLIENT_URL = process.env.CLIENT_BASE_URL || 'http://localhost';
 const PORT = process.env.PORT || 3001;
-const app = express();
+const CLIENT_PORT = process.env.CLIENT_PORT || 3000;
 
-app.use(cors());
+const corsOptions = {
+  origin: `${CLIENT_URL}:${CLIENT_PORT}`,
+  optionsSuccessStatus: 200,
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(session({
   secret: 'keyboard cat',
@@ -22,7 +31,29 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(router);
 
-app.listen(PORT, (err) => {
+const server = app.listen(PORT, (err) => {
   if (err) console.log(err);
   else console.log(`Server listening at ${SERVER_URL}:${PORT}`);
+});
+
+const io = require('socket.io').listen(server);
+
+io.on('connection', (socket) => {
+  socket.broadcast.emit('hello', 'holaa');
+  console.log('user connected');
+  //TODO get code from session
+  const partyCode = '123456qwerty';
+  //listen for changes on playlist
+  socket.on(`from:${partyCode}:updatePlaylist`, (updatedPlaylist) => {
+    console.log('updated playlist', updatedPlaylist);
+    socket.broadcast.emit(`from:${partyCode}:updatePlaylist`, updatedPlaylist);
+  });
+  //listen for changes on playback
+  socket.on(`from:${partyCode}:updatePlayback`, (updatedPlayback) => {
+    console.log('updated playback', updatedPlayback);
+    socket.broadcast.emit(`from:${partyCode}:updatePlayback`, updatedPlayback);
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
